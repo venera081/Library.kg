6a01464f5f25f05f7bcd9c726efba29bdf11120f
venera081 <berekettop0010@gmail.com>
2025-10-22 14:11:56 +0600

    CW

--- CineBoard/admin.py
+++ CineBoard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tag, Review
+from .models import Tag, Review, Film
 
 admin.site.register(Tag)
 admin.site.register(Review)
+admin.site.register(Film)
